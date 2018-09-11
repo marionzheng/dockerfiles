ba93b88a15bbf342bb7e1a9bcc590a69c0c638c1
marion <marion@haoqitour.com>
2018-09-12 02:56:12 +0800

    启动命令更新

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-sed -i "s/broker.id=0/broker.id=$BROKER_ID/g" $KAFKA_CONF_DIR/server.properties
-sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=$ZK_CONNECT/g" $KAFKA_CONF_DIR/server.properties
+sed -i "s/broker.id=0/broker.id=$KAFKA_BROKER_ID/g" $KAFKA_CONF_DIR/server.properties
+sed -i "s/host.name=localhost/host.name=$KAFKA_HOST_NAME/g" $KAFKA_CONF_DIR/server.properties
+sed -i "s/log.dirs=\/tmp\/kafka-logs/log.dirs=$KAFKA_DATA_DIR/g" $KAFKA_CONF_DIR/server.properties
+sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=$ZOO_CONNECT/g" $KAFKA_CONF_DIR/server.properties
 
 exec "$@"
