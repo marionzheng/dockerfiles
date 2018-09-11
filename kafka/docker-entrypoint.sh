70db31c539db456a219c9113aa8d48dd216f7267
marion <marion@haoqitour.com>
2018-09-12 04:05:55 +0800

    启动命令更新

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -12,6 +12,8 @@ fi
 
 sed -i "s/broker.id=0/broker.id=$KAFKA_BROKER_ID/g" $KAFKA_HOME/config/server.properties
 sed -i "s/host.name=localhost/host.name=$KAFKA_HOST_NAME/g" $KAFKA_HOME/config/server.properties
+sed -i "s#listeners=PLAINTEXT://localhost:9092#listeners=PLAINTEXT://$KAFKA_HOST_NAME:9092/g" $KAFKA_HOME/config/server.properties
+sed -i "s#advertised.listeners=PLAINTEXT://localhost:9092#advertised.listeners=PLAINTEXT://$KAFKA_HOST_NAME:9092/g" $KAFKA_HOME/config/server.properties
 sed -i "s:log.dirs=/tmp/kafka-logs:log.dirs=$KAFKA_DATA_DIR:g" $KAFKA_HOME/config/server.properties
 sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=$ZOO_CONNECT/g" $KAFKA_HOME/config/server.properties
 
