845bc4cb635ada47c0a2152c8fe7c9a496acf91c
marion <marion@haoqitour.com>
2018-09-13 19:55:16 +0800

    kafka on alpine

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -12,39 +12,39 @@ fi
 # replace broker & connect configurations
 
 if [[ -x "$KAFKA_BROKER_ID" ]]; then
-    sed -i "s/broker\.id=(\d+)/broker.id=$KAFKA_BROKER_ID/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/broker.id=.*/broker.id=$KAFKA_BROKER_ID/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_LOG_DIRS" ]]; then
-    sed -i "s/log\.dirs=(.+)/log.dirs=$KAFKA_LOG_DIRS/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/log.dirs=.*/log.dirs=$KAFKA_LOG_DIRS/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_HOST_NAME" ]]; then
-    sed -i "s/host\.name=(\w+)/host.name=$KAFKA_HOST_NAME/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/host.name=.*/host.name=$KAFKA_HOST_NAME/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_PORT" ]]; then
-    sed -i "s/port=(\d+)/port=$KAFKA_PORT/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/port=.*/port=$KAFKA_PORT/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_ADVERTISED_HOST_NAME" ]]; then
-    sed -i "s/advertised\.host\.name=(\w+)/advertised.host.name=$KAFKA_ADVERTISED_HOST_NAME/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/advertised.host.name=.*/advertised.host.name=$KAFKA_ADVERTISED_HOST_NAME/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_ADVERTISED_PORT" ]]; then
-    sed -i "s/advertised\.port=(\d+)/advertised.port=$KAFKA_ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/advertised.port=.*/advertised.port=$KAFKA_ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_LISTENERS" ]]; then
-    sed -i "s/listeners=(.+)/listeners=$KAFKA_LISTENERS/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/listeners=.*/listeners=$KAFKA_LISTENERS/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_ADVERTISED_LISTENERS" ]]; then
-    sed -i "s/advertised\.listeners=(.+)/advertised.listeners=$KAFKA_ADVERTISED_LISTENERS/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/advertised.listeners=.*/advertised.listeners=$KAFKA_ADVERTISED_LISTENERS/g" $KAFKA_HOME/config/server.properties
 fi
 
 if [[ -x "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    sed -i "s/zookeeper\.connect=(.+)/zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT/g" $KAFKA_HOME/config/server.properties
+    sed -i "s/zookeeper.connect=.*/zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT/g" $KAFKA_HOME/config/server.properties
 fi
 
 exec "$@"
